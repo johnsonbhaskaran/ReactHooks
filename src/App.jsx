ffa91d8bed7b3b69ccfa4bc6a5685cc08fb9bbd5
Johnson Bhaskaran <johnsonb@live.in>
2024-08-14 08:21:56 +0530

    useState Hook eg 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,56 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [color, setColor] = useState("Black");
+  // const [brand, setBrand] = useState("Ferrari");
+  // const [model, setModel] = useState("Roma");
+  // const [year, setYear] = useState("2023");
+  // const [color, setColor] = useState("Red");
 
-  const changeColor = () => {
-    setColor("Red");
+  const [car, setCar] = useState({
+    brand: "Ferrari",
+    model: "Roma",
+    year: 2023,
+    color: "red",
+  });
+
+  const changeColor = (e) => {
+    const btnValue = e.target.innerText;
+    console.log(btnValue);
+    setCar((prev) => {
+      return { ...prev, year: 2025, color: btnValue };
+    });
   };
 
   return (
     <>
-      <h1>My favourite color is {color}!</h1>
-      <button onClick={changeColor}>Blue</button>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}
+      </p>
+      <span>
+        <button
+          style={{ backgroundColor: "mediumblue", marginRight: ".5em" }}
+          onClick={changeColor}
+        >
+          Medium Blue
+        </button>
+      </span>
+      <span>
+        <button
+          style={{ backgroundColor: "mediumorchid", marginRight: ".5em" }}
+          onClick={changeColor}
+        >
+          Orchid
+        </button>
+      </span>
+      <span>
+        <button
+          style={{ backgroundColor: "mediumseagreen" }}
+          onClick={changeColor}
+        >
+          Sea Green
+        </button>
+      </span>
     </>
   );
 };
