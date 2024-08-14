ebc2a0f033de9971ca6b3e0ed6e7e8d0bcbb106a
Johnson Bhaskaran <johnsonb@live.in>
2024-08-14 09:56:03 +0530

    useRef Hook used to select the DOM element directly to manipulate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useEffect, useState, useRef } from "react";
+import { useRef } from "react";
 import "./App.css";
 
 const App = () => {
-  const [value, setValue] = useState(0);
+  const inputElement = useRef();
 
-  const count = useRef(0);
-
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
+  const btnClicked = () => {
+    console.log(inputElement.current);
+    inputElement.current.style.backgroundColor = "white";
+  };
 
   return (
     <>
-      <button
-        onClick={() => {
-          setValue((prev) => prev - 1);
-        }}
-      >
-        -1
-      </button>
-      <h1>{value}</h1>
-      <button
-        onClick={() => {
-          setValue((prev) => prev + 1);
-        }}
-      >
-        +1
-      </button>
-      <h1>Render Count: {count.current}</h1>
+      <input
+        type='text'
+        ref={inputElement}
+      />
+      <button onClick={btnClicked}>Click Here</button>
     </>
   );
 };
