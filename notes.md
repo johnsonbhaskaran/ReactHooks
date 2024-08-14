2adaf4e4c181104ec44edeca3a371dfd6746350b
Johnson Bhaskaran <johnsonb@live.in>
2024-08-14 10:37:22 +0530

    useMemo used to momoize a value / caching a value that doesn't need to be re-calcultated

--- notes.md
+++ notes.md
@@ -6,3 +6,14 @@
 
 - It allow us to create mutable variables, which will not re-render the component.
 - Also used for accessing DOM elements
+
+## useMemo
+
+- It returns a memoized value.
+- It's like caching a value so that it doesn't need to be recalculated.
+- It runs only when one of it's dependencies gets updated.
+- It improves performance.
+
+- useMemo is similar to useCallback. It also improves performance.
+- but useMemo returns memoized value
+- and useCallback returns a memoized function.
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useRef } from "react";
+import { useState, useMemo } from "react";
 import "./App.css";
 
 const App = () => {
-  const inputElement = useRef();
+  const [number, setNumber] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const btnClicked = () => {
-    console.log(inputElement.current);
-    inputElement.current.style.backgroundColor = "white";
-  };
+  function cubeNum(num) {
+    console.log("Calculation done!");
+    return Math.pow(num, 3);
+  }
+
+  const result = useMemo(() => {
+    return cubeNum(number);
+  }, [number]);
 
   return (
     <>
       <input
-        type='text'
-        ref={inputElement}
+        type='number'
+        value={number}
+        onChange={(e) => {
+          setNumber(e.target.value);
+        }}
       />
-      <button onClick={btnClicked}>Click Here</button>
+      <h1>Cube of the number: {result}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter++
+      </button>
+      <h1>Counter: {counter}</h1>
     </>
   );
 };
